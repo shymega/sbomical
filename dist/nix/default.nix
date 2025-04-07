94b05efd7c8384cae94176fbae395d8c3a34090c
Dom Rodriguez <shymega@shymega.org.uk>
2025-04-08 00:15:41 +0100

    refactor: Refactor package to use `finalAttrs`

--- dist/nix/default.nix
+++ dist/nix/default.nix
@@ -1,30 +1,26 @@
 # SPDX-FileCopyrightText: 2025 Dom Rodriguez <Dom.Rodriguez@codethink.co.uk>
 #
 # SPDX-License-Identifier: Apache-2.0
-{ rustPlatform
-, lib
-, self
+{
+  rustPlatform,
+  lib,
+  self,
 }:
-let
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "sbomical";
-  version = "0.1.0-unstable";
+  version = "unstable";
 
-  inherit (rustPlatform) buildRustPackage;
-in
-buildRustPackage rec {
-    inherit pname version;
+  src = lib.cleanSource self;
 
-    src = lib.cleanSource self;
+  cargoLock = {
+    lockFile = "${finalAttrs.src}/Cargo.lock";
+  };
 
-    cargoLock = {
-      lockFile = "${src}/Cargo.lock";
-    };
-
-    meta = with lib; {
-      description = "Utility for scanning software projects, and generating a SPDX-compliant SBOM.";
-      homepage = "https://github.com/shymega/sbomical";
-      license = with licenses; asl20;
-      maintainers = with maintainers; [ shymega ];
-      mainProgram = "sbomical";
-    };
-}
+  meta = {
+    description = "Suite for scanning software projects and generating SBOMs, as well as license reports.";
+    homepage = "https://github.com/shymega/sbomical";
+    license = lib.licenses.asl20;
+    maintainers = with maintainers; [shymega];
+    mainProgram = "sbomical";
+  };
+})
