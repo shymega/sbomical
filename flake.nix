8b43d3f72aef7f9eef41be6629d5077dedb40f40
Dom Rodriguez <shymega@shymega.org.uk>
2025-04-08 00:12:25 +0100

    refactor: Add devenv support to Flake

--- flake.nix
+++ flake.nix
@@ -1,36 +1,42 @@
 # SPDX-FileCopyrightText: 2025 Dom Rodriguez <Dom.Rodriguez@codethink.co.uk>
 #
 # SPDX-License-Identifier: Apache-2.0
-
-{ 
+{
   description = "Suite for scanning software projects and generating SBOMs, as well as license reports.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
+    devenv.url = "github:cachix/devenv";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" "riscv64-linux" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      });
-    in
+  outputs = inputs: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" "riscv64-linux"];
+    inherit (inputs) self;
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+          };
+        });
+  in
     {
-      packages = forEachSupportedSystem ({ pkgs }:{
+      packages = forEachSupportedSystem ({pkgs}: {
         default = self.packages.${pkgs.system}.sbomical;
-        sbomical = pkgs.callPackage ./dist/nix { inherit self; };
+        sbomical = pkgs.callPackage ./dist/nix {inherit self;};
+        devenv-up = self.devShells.${pkgs.system}.default.config.procfileScript;
+        devenv-test = self.devShells.${pkgs.system}.default.config.test;
       });
 
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${pkgs.system}.sbomical ];
+      devShells = forEachSupportedSystem ({pkgs}: {
+        default = inputs.devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [./dist/nix/devenv];
         };
       });
-    } // {
-      overlays.default = final: prev: {
+    }
+    // {
+      overlays.default = final: _: {
         inherit (self.packages.${final.system}) sbomical;
       };
     };
